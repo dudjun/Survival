35e2bfabe62b64c7a316b0295c9e36078ed17628
dudjun <dud4817@naver.com>
2022-05-05 01:15:50 +0900

    2022-05-05
    Lesson 1 Moving Player
    Lesson 2 Moving Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 스피드 조정 변수
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float crouchSpeed;
+
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    // 상태 변수
+    private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = true;
+
+    // 앉았을 때 얼마나 앉을지
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    // 땅 착지 여부 확인용
+    private CapsuleCollider capsuleCollider;
+
+    // 민감도
+    [SerializeField]
+    private float lookSensitivity; 
+
+    // 카메라 한계
+    [SerializeField]
+    private float cameraRotationLimit; 
+    private float currentCameraRotationX = 0f;
+
+    // 컴포넌트
+    [SerializeField]
+    private Camera theCamera;
+    private Rigidbody myRigid;
+
+    private void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
+    }
+
+    private void Update()
+    {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    IEnumerator CrouchCoroutine()
+    {
+
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+        while(_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+                break;
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (isCrouch)
+            Crouch();
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running()
+    {
+        if (isCrouch)
+            Crouch();
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");  // ad 혹은 좌우 화살표를 누르면 1과 -1을 리턴하고 안누르면 0 리턴함
+        float _moveDirZ = Input.GetAxisRaw("Vertical");    // ws 혹은 위아래 화살표를 누르면 1과 -1을 리턴하고 안누르면 0 리턴함
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;  // d눌렀을 경우 (1, 0, 0)
+        Vector3 _moveVertical = transform.forward * _moveDirZ;  // w눌렀을 경우 (0, 0, 1)
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+    private void CharacterRotation()
+    {
+        // 좌우 캐릭터 회전
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));  // myRigid.rotation은 쿼터니언이기 때문에 오일러인 _characterRotationY를 쿼터니언으로 바꿔준다.
+    }
+
+    private void CameraRotation()
+    {
+        // 상하 카메라 회전
+        float _xRotation = Input.GetAxisRaw("Mouse Y");  // Mouse Y인 이유: 마우스 움직임은 z축이 없는 3차원이다.
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);  // currentCameraRotationX 값을 제한한다.
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+}
