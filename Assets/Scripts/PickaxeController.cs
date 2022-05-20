9ae552a5264b7a4032239c5fbd24264b528fb2d1
dudjun <dud4817@naver.com>
2022-05-21 02:36:52 +0900

    2022-05-21
    Lesson 14 SoundManager
    Lesson 15 나뭇가지 베기

--- Assets/Scripts/PickaxeController.cs
+++ Assets/Scripts/PickaxeController.cs
@@ -29,6 +29,10 @@ public class PickaxeController : CloseWeaponController
                 {
                     hitInfo.transform.GetComponent<Rock>().Mining();
                 }
+                else if (hitInfo.transform.tag == "Twig")
+                {
+                    hitInfo.transform.GetComponent<Twig>().Damage(this.transform);
+                }
                 isSwing = false;
                 Debug.Log(hitInfo.transform.name);
             }
